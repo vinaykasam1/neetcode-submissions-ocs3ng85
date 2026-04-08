6669670ac4ca807b13fad8b47b2854bdd6a5fee0
vinaykasam1 <vinaykasam1@gmail.com>
2026-04-08 23:25:35 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashMap = {}
+        
+        for i,num in enumerate(nums):
+            diff = target - num
+            
+            if diff in hashMap:
+                return [hashMap[diff],i]
+            
+            hashMap[num] = i
+    
